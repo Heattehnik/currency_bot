6863b759615d6e9f149ae674276e3c321e2bf880
Heattehnik <roman.ecm@mail.ru>
2023-11-17 20:34:01 +0500

    Add serializer, view, url in currency app.

--- backend/currency/urls.py
+++ backend/currency/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from currency.apps import CurrencyConfig
+from currency.views import LatestCurrencyView
 
 app_name = CurrencyConfig.name
 
 urlpatterns = [
-
+    path('currency', LatestCurrencyView.as_view(), name='latest_currency'),
+    # Другие URL-шаблоны вашего приложения...
 ]
--- backend/currency/views.py
+++ backend/currency/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from clients.models import Client
+from request_history.models import Request
+from .models import Currency
+from .serializers import CurrencySerializer
+
+
+class LatestCurrencyView(APIView):
+
+    def get(self, request):
+        queryset = Currency.objects.latest('date')
+        user_id = request.query_params.get('user_id')
+        client = Client.objects.get(user_id=user_id)
+        request_obj = Request(user_id=client, currency=queryset)
+        request_obj.save()
+        serializer = CurrencySerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
