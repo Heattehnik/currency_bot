a70de75b42603ec7bf702d9262f6329f188b7589
Heattehnik <roman.ecm@mail.ru>
2023-11-17 20:35:03 +0500

    Add default pagination.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -119,9 +119,8 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
       'rest_framework.permissions.AllowAny',
     ]
