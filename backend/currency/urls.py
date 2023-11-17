31891a4a9326ca33e4eb028d8168fc5ba1f692bd
roman <roman.ecm@mail.ru>
2023-11-17 15:25:03 +0500

    Add urls for apps

---
+++ backend/currency/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from currency.apps import CurrencyConfig
+
+app_name = CurrencyConfig.name
+
+urlpatterns = [
+
+]
---
+++ backend/request_history/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from request_history.apps import RequestHistoryConfig
+
+app_name = RequestHistoryConfig.name
+
+urlpatterns = [
+
+]
