a40da7651bcc8bd83678fd081519378530733cf5
Heattehnik <roman.ecm@mail.ru>
2023-11-17 20:34:32 +0500

    Add serializer, view, url in request_history app.

--- backend/request_history/urls.py
+++ backend/request_history/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from request_history.apps import RequestHistoryConfig
+from request_history.views import UserRequestsView
 
 app_name = RequestHistoryConfig.name
 
 urlpatterns = [
-
+    path('history/', UserRequestsView.as_view(), name='history'),
 ]
--- backend/request_history/views.py
+++ backend/request_history/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Request
+from .serializers import RequestSerializer
 
-# Create your views here.
+
+class UserRequestsView(APIView):
+    def get(self, request):
+        user_id = request.query_params.get('user_id')  # Получаем user_id из query параметров
+
+        # Получаем объекты Request, совпадающие с user_id
+        requests = Request.objects.filter(user_id=user_id)[:5]
+        serializer = RequestSerializer(requests, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
