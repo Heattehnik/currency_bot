5646cb51472b5aa8b6eb693113f93ec27097d619
roman <roman.ecm@mail.ru>
2023-11-22 16:25:37 +0500

    refactor tasks.py

--- backend/clients/tasks.py
+++ backend/clients/tasks.py
@@ -9,10 +9,13 @@ bot = telebot.TeleBot(token=os.getenv('BOT_TOKEN'))
 
 @shared_task
 def send_currency_rate():
-    currency_rate = Currency.objects.latest('date')
-    subscribed_users = Client.objects.filter(is_subscribed=True)
-    for user in subscribed_users:
-        chat_id = user.user_id
-        message = f"{currency_rate}"
-        bot.send_message(chat_id, message)
+    try:
+        currency_rate = Currency.objects.latest('date')
+        subscribed_users = Client.objects.filter(is_subscribed=True)
+        for user in subscribed_users:
+            chat_id = user.user_id
+            message = f"{currency_rate}"
+            bot.send_message(chat_id, message)
+    except Currency.DoesNotExist:
+        return 'Нет курса валют'
     return "Курс доллара отправлен подписчикам"
